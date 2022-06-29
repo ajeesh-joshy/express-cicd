136022809bf447dc65819fa5b752f73d83ab99ad
Ajeesh Joshy <ajeesh.joshy@ignitarium.com>
2022-06-29 17:49:46 +0530

    First commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { createServer } from './server';
+
+const port = 3000
+
+const app = createServer();
+
+app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`App listening on the port ${port}`);    
+})
---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import routes from './routes';
+
+export function createServer() {
+    const app = express();
+
+    app.use(routes);
+
+    return app;
+}
